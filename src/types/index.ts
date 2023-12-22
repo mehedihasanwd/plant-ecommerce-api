83058f570932bc0c242e15d7d77ce196c33f1902
Mehedi Hasan <hellomehediwd@gmail.com>
2023-12-22 23:03:22 +0600

    Import and export staff_type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,6 @@
 import * as common_type from "./common_type";
 import * as payload_type from "./payload_type";
 import * as document_method from "./document_method";
+import * as staff_type from "./staff_type";
 
-export { common_type, payload_type, document_method };
+export { common_type, payload_type, document_method, staff_type };
