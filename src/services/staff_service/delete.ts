b2aa839332aa8a6ef9f4bd5732330c1e05bae72f
Mehedi Hasan <hellomehediwd@gmail.com>
2023-12-23 15:09:54 +0600

    Remode delete.ts staff service

--- src/services/staff_service/delete.ts
+++
@@ -1,8 +0,0 @@
-import { Staff } from "../../models";
-import { staff_type } from "../../types";
-
-export const removeStaffById = async ({
-  _id,
-}: staff_type.IRemoveStaffById): Promise<staff_type.TRemovedStaffDocument> => {
-  return await Staff.findByIdAndDelete(_id);
-};
