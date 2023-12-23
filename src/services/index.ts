5e4b59112571c6962f05460a8e9f559f54ad2d9e
Mehedi Hasan <hellomehediwd@gmail.com>
2023-12-23 21:54:49 +0600

    Edit staff service import type

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,4 @@
 import * as redis_service from "./redis_service";
-import * as staff_service from "./staff_service";
+import staff_service from "./staff_service";
 
 export { redis_service, staff_service };
