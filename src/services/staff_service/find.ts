15667a0cbe063e07c4b572963853b5bfd8ccdc1b
Mehedi Hasan <hellomehediwd@gmail.com>
2023-12-23 14:42:01 +0600

    Add findStaffByProp, isMatchStaffPassword, findStaffs, countStaffs services

---
+++ src/services/staff_service/find.ts
@@ -0,0 +1,112 @@
+import { Staff } from "../../models";
+import { staff_type, common_type } from "../../types";
+
+export const findStaffByProp = async ({
+  key,
+  value,
+}: staff_type.IFindStaffByProp): Promise<staff_type.THydratedStaffDocument | null> => {
+  if (key === "_id") {
+    return await Staff.findById(value);
+  }
+  return await Staff.findOne({ [key]: value });
+};
+
+export const isMatchStaffPassword = async ({
+  staff,
+  password,
+}: staff_type.IMatchStaffPassword): Promise<boolean> => {
+  return await staff.comparePassword({ password });
+};
+
+export const findStaffs = async ({
+  skip,
+  limit,
+  sort_type,
+  search_by,
+}: staff_type.IFindStaffs): Promise<Array<staff_type.THydratedStaffDocument> | null> => {
+  let staffs: Array<staff_type.THydratedStaffDocument> | null = [];
+
+  if (skip && limit) {
+    staffs = await Staff.aggregate([
+      {
+        $sort: { _id: sort_type === "asc" ? 1 : -1 },
+      },
+
+      {
+        $skip: skip,
+      },
+
+      { $limit: limit },
+    ]);
+  }
+
+  if (skip && limit && search_by) {
+    staffs = await Staff.aggregate([
+      {
+        $sort: { _id: sort_type === "asc" ? 1 : -1 },
+      },
+
+      {
+        $match: {
+          $or: [
+            { name: { $regex: search_by, $options: "i" } },
+            { email: { $regex: search_by, $options: "i" } },
+            { phone: { $regex: search_by, $options: "i" } },
+          ],
+        },
+      },
+
+      {
+        $skip: skip,
+      },
+
+      { $limit: limit },
+    ]);
+  }
+
+  if (!skip && limit && search_by) {
+    staffs = await Staff.aggregate([
+      { $sort: { _id: sort_type === "asc" ? 1 : -1 } },
+
+      {
+        $match: {
+          $or: [
+            { name: { $regex: search_by, $options: "i" } },
+            { email: { $regex: search_by, $options: "i" } },
+            { phone: { $regex: search_by, $options: "i" } },
+          ],
+        },
+      },
+
+      { $limit: limit },
+    ]);
+  }
+
+  if (!skip && !limit && !search_by) {
+    staffs = await Staff.aggregate([
+      { $sort: { _id: sort_type === "asc" ? 1 : -1 } },
+    ]);
+  }
+
+  if (!staffs || staffs.length < 1) return null;
+
+  return staffs;
+};
+
+export const countStaffs = async ({
+  search_by,
+}: common_type.ISearchBy): Promise<number> => {
+  const filter = search_by
+    ? [
+        {
+          $or: [
+            { name: { $regex: search_by, $options: "i" } },
+            { email: { $regex: search_by, $options: "i" } },
+            { phone: { $regex: search_by, $options: "i" } },
+          ],
+        },
+      ]
+    : {};
+
+  return await Staff.countDocuments(filter);
+};
