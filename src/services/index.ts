e1be5700b5570cac81d3eafa04618fb7a781cbd6
Mehedi Hasan <hellomehediwd@gmail.com>
2023-12-24 16:03:54 +0600

    Import and export aws_s3 service

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,6 @@
 import * as redis_service from "./redis_service";
 import staff_service from "./staff_service";
+import * as mailer from "./mailer";
+import * as aws_s3 from "./amazon_s3";
 
-export { redis_service, staff_service };
+export { redis_service, staff_service, mailer, aws_s3 };
