8dfd917f788bf25bab7a6ff399ce3dd59ce146ef
Mehedi Hasan <hellomehediwd@gmail.com>
2023-12-25 12:59:15 +0600

    Assign staff controllers in staff auth routes

--- src/routes/staff/auth.ts
+++ src/routes/staff/auth.ts
@@ -1,4 +1,7 @@
 import express from "express";
+import { staff_controller } from "../../controllers";
+import { setLimiter } from "../../utils";
+import * as authorization from "../../middleware/authenticate";
 
 const router: express.Router = express.Router();
 
@@ -29,33 +32,68 @@ const staff_auth_route: IStaffAuthRoute = {
 };
 
 // verify-email
-router.post(staff_auth_route.verifyEmail, () => {});
+router.post(
+  staff_auth_route.verifyEmail,
+  setLimiter(10),
+  staff_controller.postVerifyEmail
+);
 
 // register
-router.post(staff_auth_route.register, () => {});
+router.post(
+  staff_auth_route.register,
+  setLimiter(10),
+  staff_controller.postRegisterStaff
+);
 
 // login
-router.post(staff_auth_route.login, () => {});
+router.post(staff_auth_route.login, staff_controller.postLoginStaff);
 
 // logout
-router.post(staff_auth_route.logout, () => {});
+router.post(
+  staff_auth_route.logout,
+  authorization.authorizeStaffRolesAndSelf,
+  staff_controller.postLogoutStaff
+);
 
 // forgot-password
-router.post(staff_auth_route.forgotPassword, () => {});
+router.post(
+  staff_auth_route.forgotPassword,
+  setLimiter(15),
+  staff_controller.postForgotPassword
+);
 
 // reset-password
-router.patch(staff_auth_route.resetPassword, () => {});
+router.patch(
+  staff_auth_route.resetPassword,
+  staff_controller.patchResetStaffPassword
+);
 
 // change-password
-router.patch(staff_auth_route.changePassword, () => {});
+router.patch(
+  staff_auth_route.changePassword,
+  authorization.authorizeStaffSelf,
+  staff_controller.patchChangeStaffPassword
+);
 
 // verify-staff-email
-router.post(staff_auth_route.verifyStaffEmail, () => {});
+router.post(
+  staff_auth_route.verifyStaffEmail,
+  authorization.authorizeStaffSelf,
+  staff_controller.postVerifyStaffEmail
+);
 
 // update-staff-email
-router.patch(staff_auth_route.updateStaffEmail, () => {});
+router.patch(
+  staff_auth_route.updateStaffEmail,
+  authorization.authorizeStaffSelf,
+  staff_controller.patchUpdateStaffEmail
+);
 
 // get new access_token
-router.get(staff_auth_route.accessToken, () => {});
+router.get(
+  staff_auth_route.accessToken,
+  authorization.authorizeStaffSelf,
+  staff_controller.getNewAccessToken
+);
 
 export default router;
