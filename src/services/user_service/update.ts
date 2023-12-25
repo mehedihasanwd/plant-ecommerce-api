294b9ca6e5b9d1d95bd9b570e8bf3d7acc606cd5
Mehedi Hasan <hellomehediwd@gmail.com>
2023-12-25 22:01:14 +0600

    Add user update services

---
+++ src/services/user_service/update.ts
@@ -0,0 +1,38 @@
+import { User } from "../../models";
+import { user_type } from "../../types";
+
+export const updateUserById = async ({
+  _id,
+  data,
+}: user_type.IUpdateUserById): Promise<user_type.THydratedUserDocument | null> => {
+  const updated_user: user_type.THydratedUserDocument | null =
+    await User.findByIdAndUpdate(
+      _id,
+
+      {
+        $set: { ...data },
+      },
+
+      { new: true }
+    );
+
+  if (!updated_user) return null;
+
+  return updated_user.save();
+};
+
+export const updateUserPassword = async ({
+  user,
+  new_password,
+}: user_type.IUpdateUserPassword): Promise<user_type.THydratedUserDocument | null> => {
+  user.password = new_password;
+  return user.save();
+};
+
+export const updateUserEmail = async ({
+  user,
+  new_email,
+}: user_type.IUpdateUserEmail): Promise<user_type.THydratedUserDocument | null> => {
+  user.email = new_email;
+  return user.save();
+};
