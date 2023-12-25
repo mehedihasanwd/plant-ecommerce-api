0fe07437221a7b907bba54e2f9602b6b25e18ad7
Mehedi Hasan <hellomehediwd@gmail.com>
2023-12-25 21:28:48 +0600

    Configure staff crud routes

--- src/routes/staff/crud.ts
+++ src/routes/staff/crud.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import { staff_controller } from "../../controllers";
+import * as authorization from "../../middleware/authenticate";
 
 const router: express.Router = express.Router();
 
@@ -13,9 +15,24 @@ const crud_route: ICrudRoute = {
 };
 
 // get staffs
-router.get(crud_route.base_path, () => {});
+router.get(
+  crud_route.base_path,
+  authorization.authorizeStaff,
+  staff_controller.getStaffs
+);
 
 // staff by id
-router.route(crud_route.staff_by_id).get().patch().put().delete();
+router
+  .route(crud_route.staff_by_id)
+  .get(authorization.authorizeStaff, staff_controller.getStaffById)
+  .patch(
+    authorization.authorizeSuperAdmin,
+    staff_controller.patchUpdateStaffRoleById
+  )
+  .put(authorization.authorizeStaffSelf, staff_controller.putUpdateStaffById)
+  .delete(
+    authorization.authorizeSuperAdminOrStaffSelf,
+    staff_controller.deleteStaffById
+  );
 
 export default router;
