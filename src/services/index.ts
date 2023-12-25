c581e4fe79132e72c25b6fba7a33e4a3d2d5bac6
Mehedi Hasan <hellomehediwd@gmail.com>
2023-12-25 22:03:28 +0600

    Import and export user service

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,6 +1,7 @@
 import * as redis_service from "./redis_service";
 import staff_service from "./staff_service";
+import user_service from "./user_service";
 import * as mailer from "./mailer";
 import * as aws_s3 from "./amazon_s3";
 
-export { redis_service, staff_service, mailer, aws_s3 };
+export { redis_service, staff_service, user_service, mailer, aws_s3 };
