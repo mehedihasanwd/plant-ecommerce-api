d578a2c22dfd98ca41f879b537a51d0299f594d9
Mehedi Hasan <hellomehediwd@gmail.com>
2023-12-25 21:39:28 +0600

    Import and export user type

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,5 +2,6 @@ import * as common_type from "./common_type";
 import * as payload_type from "./payload_type";
 import * as document_method from "./document_method";
 import * as staff_type from "./staff_type";
+import * as user_type from "./user_type";
 
-export { common_type, payload_type, document_method, staff_type };
+export { common_type, payload_type, document_method, staff_type, user_type };
