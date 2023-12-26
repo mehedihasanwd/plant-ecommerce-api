4bd101c33cee570fe8ff5618e5d12a0ea0fe594e
Mehedi Hasan <hellomehediwd@gmail.com>
2023-12-26 13:55:09 +0600

    Add staff patch controller

--- src/controllers/user_controller/patch.ts
+++ src/controllers/user_controller/patch.ts
@@ -1,24 +1,23 @@
 import jwt from "jsonwebtoken";
 import { RequestHandler } from "express";
-import { staff_service, redis_service } from "../../services";
-import { staff_validator } from "../../validators";
+import { user_service, redis_service } from "../../services";
+import { user_validator } from "../../validators";
 import {
   response,
   errorReducer,
   document_extractor,
   create_cache_key,
-  isValidParamId,
 } from "../../utils";
-import { common_type, staff_type } from "../../types";
+import { common_type, user_type } from "../../types";
 import dotenvconfig from "../../config/dotenvconfig";
 
-export const patchResetStaffPassword: RequestHandler = async (
+export const patchResetUserPassword: RequestHandler = async (
   req,
   res,
   next
 ) => {
   const { value, error } =
-    staff_validator.reset_password_validate_schema.validate({
+    user_validator.reset_password_validate_schema.validate({
       token: req.body?.token,
     });
 
@@ -45,44 +44,45 @@ export const patchResetStaffPassword: RequestHandler = async (
 
         const decoded_token = decoded as common_type.IForgotPassword;
 
-        const staff: staff_type.THydratedStaffDocument | null =
-          await staff_service.findStaffByProp({
+        const user: user_type.THydratedUserDocument | null =
+          await user_service.findUserByProp({
             key: "email",
             value: decoded_token.email,
           });
 
-        if (!staff) {
+        if (!user) {
           return response.responseErrorMessage(res, 404, {
             error: "There is no account exists with this email!",
           });
         }
 
-        const updated_staff: staff_type.THydratedStaffDocument | null =
-          await staff_service.updateStaffPassword({
-            staff,
+        const updated_user: user_type.THydratedUserDocument | null =
+          await user_service.updateUserPassword({
+            user,
             new_password: value.new_password,
           });
 
-        if (!updated_staff) {
+        if (!updated_user) {
           return response.responseErrorMessage(res, 500, {
             error: "Server error occurred! please try again",
           });
         }
 
         const { access_token, refresh_token } =
-          await staff_service.createStaffAuthTokenTokens({
-            staff: updated_staff,
+          await user_service.createUserAuthTokens({
+            user: updated_user,
           });
 
-        const { data_except_password } =
-          document_extractor.extractStaffDocument({ staff: updated_staff });
+        const { data_except_password } = document_extractor.extractUserDocument(
+          { user: updated_user }
+        );
 
-        // clear staffs from cache
-        await redis_service.clearKeys({ key: "staffs" });
+        // clear users from cache
+        await redis_service.clearKeys({ key: "users" });
 
-        // save staff in cache (3600 * 24 * 2 = for 2 days)
+        // save user in cache (3600 * 24 * 2 = for 2 days)
         const cache_key: string = create_cache_key.createKeyForDocument({
-          key: "staff",
+          key: "user",
           value: data_except_password._id.toString(),
         });
 
@@ -100,10 +100,10 @@ export const patchResetStaffPassword: RequestHandler = async (
             code: 200,
             message: "Password reset successfully",
             access_token,
-            staff: data_except_password,
+            user: data_except_password,
             links: {
-              self: "/auth/staffs/reset-password",
-              profile: `/staffs/s/${data_except_password._id}/profile`,
+              self: "/auth/users/reset-password",
+              profile: `/users/u/${data_except_password._id}/profile`,
             },
           }
         );
@@ -114,13 +114,13 @@ export const patchResetStaffPassword: RequestHandler = async (
   }
 };
 
-export const patchChangeStaffPassword: RequestHandler = async (
+export const patchChangeUserPassword: RequestHandler = async (
   req,
   res,
   next
 ) => {
   const { value, error } =
-    staff_validator.change_password_validate_schema.validate({
+    user_validator.change_password_validate_schema.validate({
       email: req.body?.email,
       password: req.body?.password,
       new_password: req.body?.new_password,
@@ -133,18 +133,19 @@ export const patchChangeStaffPassword: RequestHandler = async (
   }
 
   try {
-    const staff: staff_type.THydratedStaffDocument | null =
-      await staff_service.findStaffByProp({ key: "email", value: value.email });
+    const user: user_type.THydratedUserDocument | null =
+      await user_service.findUserByProp({ key: "email", value: value.email });
 
-    if (!staff) {
+    if (!user) {
       return response.responseErrorMessage(res, 404, {
         error: "There is no account exists with this email!",
       });
     }
 
-    const is_match_password: boolean = await staff_service.isMatchStaffPassword(
-      { staff, password: value.password }
-    );
+    const is_match_password: boolean = await user_service.isMatchUserPassword({
+      user,
+      password: value.password,
+    });
 
     if (!is_match_password) {
       return response.responseErrorMessage(res, 401, {
@@ -152,29 +153,29 @@ export const patchChangeStaffPassword: RequestHandler = async (
       });
     }
 
-    const updated_staff: staff_type.THydratedStaffDocument | null =
-      await staff_service.updateStaffPassword({
-        staff,
+    const updated_user: user_type.THydratedUserDocument | null =
+      await user_service.updateUserPassword({
+        user,
         new_password: value.new_password,
       });
 
-    if (!updated_staff) {
+    if (!updated_user) {
       return response.responseErrorMessage(res, 500, {
         error: "Server error occurred! please try again",
       });
     }
 
-    const { data_except_password } = document_extractor.extractStaffDocument({
-      staff: updated_staff,
+    const { data_except_password } = document_extractor.extractUserDocument({
+      user: updated_user,
     });
 
     return response.responseSuccessData(res, 200, {
       code: 200,
       message: "Password changed successfully",
-      staff: data_except_password,
+      user: data_except_password,
       links: {
-        self: "/auth/staffs/change-passwor",
-        profile: `/staffs/s/${data_except_password._id}/profile`,
+        self: "/auth/users/change-passwor",
+        profile: `/users/u/${data_except_password._id}/profile`,
       },
     });
   } catch (e) {
@@ -182,9 +183,9 @@ export const patchChangeStaffPassword: RequestHandler = async (
   }
 };
 
-export const patchUpdateStaffEmail: RequestHandler = async (req, res, next) => {
+export const patchUpdateUserEmail: RequestHandler = async (req, res, next) => {
   const { value, error } =
-    staff_validator.update_staff_email_validate_schema.validate({
+    user_validator.update_user_email_validate_schema.validate({
       token: req.body?.token,
     });
 
@@ -209,45 +210,46 @@ export const patchUpdateStaffEmail: RequestHandler = async (req, res, next) => {
           });
         }
 
-        const decoded_token = decoded as staff_type.IPostVerifyStaffEmail;
+        const decoded_token = decoded as user_type.IPostVerifyUserEmail;
 
-        const staff: staff_type.THydratedStaffDocument | null =
-          await staff_service.findStaffByProp({
+        const user: user_type.THydratedUserDocument | null =
+          await user_service.findUserByProp({
             key: "email",
             value: decoded_token.email,
           });
 
-        if (!staff) {
+        if (!user) {
           return response.responseErrorMessage(res, 404, {
             error: "There is no account exists with this email!",
           });
         }
 
-        const updated_staff: staff_type.THydratedStaffDocument | null =
-          await staff_service.updateStaffEmail({
-            staff,
+        const updated_user: user_type.THydratedUserDocument | null =
+          await user_service.updateUserEmail({
+            user,
             new_email: decoded_token.new_email,
           });
 
-        if (!updated_staff) {
+        if (!updated_user) {
           return response.responseErrorMessage(res, 500, {
             error: "Server error occurred! please try again",
           });
         }
 
-        const { access_token } = await staff_service.createStaffAuthTokenTokens(
-          { staff: updated_staff }
+        const { access_token } = await user_service.createUserAuthTokens({
+          user: updated_user,
+        });
+
+        const { data_except_password } = document_extractor.extractUserDocument(
+          { user: updated_user }
         );
 
-        const { data_except_password } =
-          document_extractor.extractStaffDocument({ staff: updated_staff });
+        // clear users from cache
+        await redis_service.clearKeys({ key: "users" });
 
-        // clear staffs from cache
-        await redis_service.clearKeys({ key: "staffs" });
-
-        // save staff in cache (3600 * 24 * 2 - for 2 days)
+        // save user in cache (3600 * 24 * 2 - for 2 days)
         const cache_key: string = create_cache_key.createKeyForDocument({
-          key: "staff",
+          key: "user",
           value: data_except_password._id.toString(),
         });
 
@@ -261,90 +263,14 @@ export const patchUpdateStaffEmail: RequestHandler = async (req, res, next) => {
           code: 200,
           message: "Email updated successfully",
           access_token,
-          staff: data_except_password,
+          user: data_except_password,
           links: {
-            self: "/auth/staffs/update-staff-email",
-            profile: `/staffs/s/${data_except_password._id}/profile`,
+            self: "/auth/users/update-user-email",
+            profile: `/users/u/${data_except_password._id}/profile`,
           },
         });
       }
     );
-  } catch (e) {
-    return next(e);
-  }
-};
-
-export const patchUpdateStaffRoleById: RequestHandler = async (
-  req,
-  res,
-  next
-) => {
-  const _id = req.params?.staffId;
-
-  const { value, error } =
-    staff_validator.update_staff_role_validate_schema.validate({
-      role: req.body?.role,
-    });
-
-  if (!isValidParamId(_id)) {
-    return response.responseErrorMessage(res, 401, {
-      error: "Invalid staff id! please try again using a valid one",
-    });
-  }
-
-  if (error) {
-    return response.responseErrorMessage(res, 400, {
-      error: error.details[0].message,
-    });
-  }
-
-  try {
-    const staff: staff_type.THydratedStaffDocument | null =
-      await staff_service.findStaffByProp({ key: "_id", value: _id });
-
-    if (!staff) {
-      return response.responseErrorMessage(res, 404, {
-        error: "Staff not found!",
-      });
-    }
-
-    const updated_staff: staff_type.THydratedStaffDocument | null =
-      await staff_service.updateStaffRoleById({ _id, role: value.role });
-
-    if (!updated_staff) {
-      return response.responseErrorMessage(res, 500, {
-        error: "Server error occurred! please try again",
-      });
-    }
-
-    const { data_except_password } = document_extractor.extractStaffDocument({
-      staff: updated_staff,
-    });
-
-    // clear staffs from cache
-    await redis_service.clearKeys({ key: "staffs" });
-
-    // save staff in cache (3600 * 24 * 2 - for 2 days)
-    const cache_key: string = create_cache_key.createKeyForDocument({
-      key: "staff",
-      value: data_except_password._id.toString(),
-    });
-
-    await redis_service.saveToCache({
-      key: cache_key,
-      document: data_except_password,
-      EX: 3600 * 25 * 2,
-    });
-
-    return response.responseSuccessData(res, 200, {
-      code: 200,
-      message: "Role updated successfully",
-      staff: data_except_password,
-      links: {
-        self: `/staffs/s/${_id}`,
-        staffs: "/dashboard/staffs",
-      },
-    });
   } catch (e) {
     return next(e);
   }
