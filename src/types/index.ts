7fe045216aacf9e4f137d18954416c463c256e56
Mehedi Hasan <hellomehediwd@gmail.com>
2023-12-31 11:53:08 +0600

    Import and export category type

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,5 +3,13 @@ import * as payload_type from "./payload_type";
 import * as document_method from "./document_method";
 import * as staff_type from "./staff_type";
 import * as user_type from "./user_type";
+import * as category_type from "./category_type";
 
-export { common_type, payload_type, document_method, staff_type, user_type };
+export {
+  common_type,
+  payload_type,
+  document_method,
+  staff_type,
+  user_type,
+  category_type,
+};
