911d3361c6fee3b77b35916a181705bd768528f2
Mehedi Hasan <hellomehediwd@gmail.com>
2023-12-31 14:42:11 +0600

    Import and export category service

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,15 @@
 import * as redis_service from "./redis_service";
 import staff_service from "./staff_service";
 import user_service from "./user_service";
+import category_service from "./category_service";
 import * as mailer from "./mailer";
 import * as aws_s3 from "./amazon_s3";
 
-export { redis_service, staff_service, user_service, mailer, aws_s3 };
+export {
+  redis_service,
+  staff_service,
+  user_service,
+  mailer,
+  aws_s3,
+  category_service,
+};
