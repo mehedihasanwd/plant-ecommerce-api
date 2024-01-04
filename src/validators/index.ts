2fe4014568e50c48f0b3edbfbf1005e6ee149e30
Mehedi Hasan <hellomehediwd@gmail.com>
2024-01-04 08:35:17 +0600

    Import and export category validator

--- src/validators/index.ts
+++ src/validators/index.ts
@@ -1,4 +1,5 @@
 import * as staff_validator from "./staff_validator";
 import * as user_validator from "./user_validator";
+import * as category_validator from "./category_validator";
 
-export { staff_validator, user_validator };
+export { staff_validator, user_validator, category_validator };
