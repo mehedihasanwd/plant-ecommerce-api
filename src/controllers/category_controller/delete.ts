d659c32ef9d14c463fd569df56e1486734d92acd
Mehedi Hasan <hellomehediwd@gmail.com>
2024-01-04 09:39:53 +0600

    Update deleteCategoryById controller

--- src/controllers/category_controller/delete.ts
+++ src/controllers/category_controller/delete.ts
@@ -1,8 +1,53 @@
-import { Category } from "../../models";
-import { category_type, common_type } from "../../types";
+import { RequestHandler } from "express";
+import { redis_service, category_service } from ".././../services";
+import {
+  create_cache_key,
+  isValidParamId,
+  response,
+  collection_keys,
+} from ".././../utils";
+import { category_type } from ".././../types";
 
-export const removeCategoryById = async ({
-  _id,
-}: common_type.IObjectIdOrString): Promise<category_type.TRemovedCategoryDocument> => {
-  return await Category.findByIdAndDelete(_id);
+export const deleteCategoryById: RequestHandler = async (req, res, next) => {
+  const _id = req.params?.categoryId;
+
+  if (!isValidParamId(_id)) {
+    return response.responseErrorMessage(res, 401, {
+      error: "Invalid category id! please try again using a valid one",
+    });
+  }
+
+  try {
+    const category: category_type.THydratedCategoryDocument | null =
+      await category_service.findCategoryByProp({ key: "_id", value: _id });
+
+    if (!category) {
+      return response.responseErrorMessage(res, 404, {
+        error: "Category not found!",
+      });
+    }
+
+    // clear categories and category from cache
+    const cache_key: string = create_cache_key.createKeyForDocument({
+      key: "category",
+      value: category._id.toString(),
+    });
+
+    await redis_service.removeFromCache({
+      key: [collection_keys.categories, cache_key],
+    });
+
+    await category_service.removeCategoryById({ _id });
+
+    return response.responseSuccessData(res, 200, {
+      code: 200,
+      message: "Deleted successfully",
+      links: {
+        self: `/categories/ca/${_id}`,
+        categories: "/categories",
+      },
+    });
+  } catch (e) {
+    return next(e);
+  }
 };
