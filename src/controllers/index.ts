65232f7d2b45ce3464b6a50c03f8a2f64d2a0530
Mehedi Hasan <hellomehediwd@gmail.com>
2024-01-04 16:26:47 +0600

    Import and export category_controller

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,4 +1,5 @@
 import staff_controller from "./staff_controller";
 import user_controller from "./user_controller";
+import category_controller from "./category_controller";
 
-export { staff_controller, user_controller };
+export { staff_controller, user_controller, category_controller };
