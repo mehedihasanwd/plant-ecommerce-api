d66447ee4da199f8fa7910ea5b36148ce7907eac
Mehedi Hasan <hellomehediwd@gmail.com>
2024-01-04 16:14:25 +0600

    Add createKeyForItems utilily

--- src/utils/create_cache_key.ts
+++ src/utils/create_cache_key.ts
@@ -27,6 +27,10 @@ interface ICreateKeyForCollection {
   search_by?: string;
 }
 
+interface ICreateKeyForItems extends ICreateKeyForCollection {
+  status: "active" | "inactive" | "all";
+}
+
 export const createKeyForDocument = ({
   key,
   value,
@@ -45,6 +49,22 @@ export const createKeyForCollection = ({
   const cache_key: string = search_by
     ? `${key}::page(${page})::limit(${limit})::skip(${skip})::sort_type(${sort_type})::search_by(${search_by})`
     : `${key}::page(${page})::limit(${limit})::skip(${skip})::sort_type(${sort_type})`;
+
+  return cache_key;
+};
+
+export const createKeyForItems = ({
+  key,
+  page,
+  limit,
+  skip,
+  sort_type,
+  search_by,
+  status,
+}: ICreateKeyForItems) => {
+  const cache_key: string = search_by
+    ? `${key}::page(${page})::limit(${limit})::skip(${skip})::sort_type(${sort_type})::search_by(${search_by})::status(${status})`
+    : `${key}::page(${page})::limit(${limit})::skip(${skip})::sort_type(${sort_type})::status(${status})`;
 
   return cache_key;
 };
