4f0e45317c6417dfc1a865e4ef1cf3833087856f
Mehedi Hasan <hellomehediwd@gmail.com>
2024-01-04 16:32:19 +0600

    Assign category controllers in category routes

--- src/routes/category.ts
+++ src/routes/category.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import * as authorization from "../middleware/authenticate";
+import { category_controller } from "../controllers";
 
 const router: express.Router = express.Router();
 
@@ -17,15 +19,37 @@ const category_route: ICategoryRoute = {
 };
 
 // post new categories, get categories
-router.route(category_route.base_path).post().get();
+router
+  .route(category_route.base_path)
+  .post(authorization.authorizeAdminStaff, category_controller.postNewCategory)
+  .get(authorization.authorizeStaff, category_controller.getCategories);
 
 // get active categories
-router.get(category_route.active_categories, () => {});
+router.get(
+  category_route.active_categories,
+  authorization.authorizeStaff,
+  category_controller.getActiveCategories
+);
 
 // get inactive categoreis
-router.get(category_route.inactive_categories, () => {});
+router.get(
+  category_route.inactive_categories,
+  authorization.authorizeStaff,
+  category_controller.getInactiveCategories
+);
 
 // category by id
-router.route(category_route.category_by_id).get().patch().put().delete();
+router
+  .route(category_route.category_by_id)
+  .get(authorization.authorizeStaff, category_controller.getCategoryById)
+  .patch(
+    authorization.authorizeStaff,
+    category_controller.patchUpdateCategoryStatusById
+  )
+  .put(authorization.authorizeStaff, category_controller.putUpdateCategoryById)
+  .delete(
+    authorization.authorizeAdminStaff,
+    category_controller.deleteCategoryById
+  );
 
 export default router;
